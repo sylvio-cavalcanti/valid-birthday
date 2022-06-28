17f7f09cdf305fad15c93126a0c991cd5919f1b1
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2022-06-28 16:22:26 -0300

    Added moment.js, and computed property checkValidBirthday

--- components/BirthdayValidation.vue
+++ components/BirthdayValidation.vue
@@ -1,8 +1,8 @@
 <template>
   <body class="bg-white">
     <div class="parent">
-      <div class="child text-center pb-24 bg-gray-100 border border-gray-100 rounded-md drop-shadow-lg">
-        <h1 class="text-center py-4">Please type in your birthday 'DD/MM/YY'.</h1>
+      <div class="child text-center pb-12 bg-gray-100 border border-gray-100 rounded-md drop-shadow-lg">
+        <h1 class="text-center py-8">Please type in your birthday <span class="text-xs font-bold"> 'DD/MM/YYYY'</span>.</h1>
         <input
           v-model="birthday"
           v-mask="'##/##/####'"
@@ -11,6 +11,8 @@
           :placeholder="$v.birthday.$error ? (!$v.birthday.required ? '* Birthday is required.' : '* Invalid birthday.') : 'Birthday'"
           @input="$v.birthday.$touch"
         />
+        
+        <div class="block mt-8 font-bold">Date is valid: <span class="text-red-500">{{checkValidBirthday}}</span></div>
       </div>
     </div>
   </body>
@@ -24,6 +26,8 @@ import {
   required,
   email
 } from 'vuelidate/lib/validators'
+import moment from 'moment'
+
 export default {
   name: 'BirthdayValidation',
   mixins: [validationMixin],
@@ -40,6 +44,24 @@ export default {
       }    
     }
   },
+  computed: {
+    checkValidBirthday (){
+      const year = moment(this.birthday, 'DD/MM/YYYY').format('YYYY')
+      const rangeStart = 1922
+      const rangeEnd = 2022
+      // Checks if the user has inserted a day, month and year
+      // Checks if the year informed is within the range predefined 
+      if ((this.birthday.length === 10) && (year >= rangeStart) && (year <= rangeEnd)) {
+        const m = moment(this.birthday, 'DD/MM/YYYY')
+        // Checks if the date is valid
+        return m.isValid()
+      }
+      else {
+        return false
+      }
+      
+    },
+  }
 };
 </script>
 
